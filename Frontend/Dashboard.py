e2b1c23814e65cd02aecf5abd0b15b1fb003eeae
Abbas <abbinira2712@gmail.com>
2021-12-09 17:19:06 +0530

    Added tkinter for Frontend and flask for server/backend

---
+++ Frontend/Dashboard.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from predefineValues import screenDisplay
+
+
+
+root = Tk()
+
+
+myLable = Label(root, text="Hello World!") 
+
+myLable.pack()
+
+screenDisplay(root)
---
+++ Frontend/predefineValues.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+
+def screenDisplay(window) :
+    window.geometry("1000x700")
+    
+
+
+    window.mainloop()
+    # c = Canvas(width=400, height=400)
+    # c.pack()
+
+    # canvas_height =20
+    # canvas_width = 30
+
+
+if __name__ == "__main__":
+    window = Tk()
+    screenDisplay(window)
