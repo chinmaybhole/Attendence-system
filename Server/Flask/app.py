eebe28a1da8cf46d981fdfcbb0bbf5e2f6d54522
Abbas <abbinira2712@gmail.com>
2021-12-09 17:23:26 +0530

    Added tkinter for Frontend and flask for server/backend

--- Server/Flask/app.py
+++ Server/Flask/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__) # name of the flask application
 
-@app.route("/")
-def hello_world():
-    return "esrdft"
+@app.route("/Dashboard")
+def dashboard():
+    return "dashboard"
+
+@app.route("/rooms")
+def rooms():
+    return "rooms"
 
 if __name__ =="__main__":
     app.run(debug=True)
