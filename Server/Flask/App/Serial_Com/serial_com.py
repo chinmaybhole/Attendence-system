d29d0bf754ca8c4d62d358ad0100f454b83412bd
Chinmay Bhole <chinmaymotog5@gmail.com>
2021-12-19 12:45:45 +0530

    Serial write code added

--- Server/Flask/App/Serial_Com/serial_com.py
+++ Server/Flask/App/Serial_Com/serial_com.py
@@ -7,3 +7,15 @@ print(decode("1A69D"))
 
 # ser = serial.Serial('COM3',9600)
 # data = ser.readline(1000)
+import serial
+import time
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+while True:
+    num = input("Enter a number: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
