8ae2cc6a44d0e3f6606a855783522c56d8b970ab
Abbas <abbinira2712@gmail.com>
2021-12-22 17:55:21 +0530

    🔥🔥

--- Server/Database/Models.py
+++ Server/Database/Models.py
@@ -1,8 +1,10 @@
 import datetime
+from pprint import pprint 
+
 
 class Users:
 
-    def __init__(self, fname, lname, uid, dept, rollno, phoneno, isStudent,passw=None,id=None):
+    def __init__(self, uid, fname, lname, passw, rollno, dept, phoneno, isStudent,id=None):
         self.id = id
         self.fname = fname
         self.lname = lname
@@ -13,38 +15,115 @@ class Users:
         self.phoneno = phoneno
         self.isStudent = isStudent
 
-    # def getProf(self):
-    #     if (self.isStudent == 0):
-    #         print(f"'{self.fname}','{self.lname}','{self.uid}','{self.dept}','{self.rollno}','{self.phoneno}'")
-    #     else:
-    #         print(f"'{self.fname}','{self.lname}','{self.uid}','{self.dept}','{self.rollno}','{self.phoneno}'")
+    def insert_user(user,conn,c):
+        with conn:
+            c.execute("INSERT INTO USERS(uid,fname,lname,passw,dept,phone,isStudent) VALUES(:uid,:fname,:lname,:passw,:dept,:phoneno,:isStudent)",{'uid': user.uid,'fname': user.fname,'lname': user.lname,'passw':user.passw,'dept':user.dept,'phoneno': user.phoneno,'isStudent': user.isStudent})
 
+    # Delete Data of table user
+    def delete_user(user,conn,c):
+        with conn:
+            c.execute("DELETE from USERS WHERE uid = :uid",{'uid': user.uid})
+
+    # Update Data of table user
+    def update_user(user,conn,c):
+        with conn:
+            c.execute(""" UPDATE USERS SET fname = :fname AND lname = :lname AND uid = :uid
+                    WHERE id = :id""",{':uid':user.uid,'id': user.id, 'fname': user.fname, 'lname': user.lname})
+
+    # get all users
+    def getAllUsers(conn,c):
+        c.execute(" SELECT * FROM USERS")
+        pprint(c.fetchall())
+
+    # get all students
+    def getStudent(conn,c):
+        c.execute(" SELECT * FROM USERS WHERE isStudent = 1")
+        pprint(c.fetchall())
+
+    # get all professors
+    def getProfessor(conn,c):
+        c.execute(" SELECT * FROM USERS WHERE isStudent = 0")
+        pprint(c.fetchall())
 
     def __repr__(self):
         return f"Users('{self.fname}','{self.lname}','{self.uid}','{self.dept}','{self.rollno}','{self.phoneno}','{self.isStudent}','{self.isProfessor}')"
 
 class Rooms:
-    def __init__(self, roomid,  timein, timeout, id =None, userid=None):
+
+    def __init__(self, roomid, roomname, userid=None,id =None):
         self.id = id
         self.roomid = roomid
         self.userid = userid
-        self.timein = timein
-        self.timeout = timeout
+        self.roomname= roomname
+       
+
+    # Insert Data Into table rooms
+    def insert_rooms(room,conn,c):
+        with conn:
+            c.execute("INSERT INTO ROOMS(userid,roomid,roomname) VALUES(:userid,:roomid,:roomname)",{'roomid': room.roomid,'userid':room.userid,'roomname':room.roomname})
+    
+    # Delete Data from table rooms
+    def delete_rooms(room,conn,c):
+        with conn:
+            c.execute("DELETE from ROOMS WHERE roomid = :roomid",{'roomid': room.roomid})
+
+    # Update Data Into table rooms
+    def update_rooms(room):
+        pass
+
+    # get all rooms
+    def getAllRooms(conn,c):
+        c.execute(" SELECT * FROM ROOMS")
+        pprint(c.fetchall())
+
+    # get one room
+    def getRoom(conn,room,c):
+        with conn:
+            c.execute(" SELECT * FROM ROOMS WHERE roomid = :roomid", {'roomid':room})
+            pprint(c.fetchall())
 
     def __repr__(self):
         return f"Room('{self.id}','{self.roomid}','{self.userid}','{self.timein}','{self.timeout}')"
 
 class Logs:
-    def __init__(self,userid,roomid,timein,timeout):
+
+    def __init__(self,userid,roomid,timein,timeout,srno=None):
+        self.srno = srno
         self.userid = userid
         self.roomid = roomid
         self.timein = timein
         self.timeout = timeout
 
+    # insert data to logs
+    def insertDataToLog(conn,c,data):
+        with conn:
+            c.execute(" INSERT INTO LOG()")
+
+    # get all logs
+    def getLogs(conn,c):
+        with conn:
+            c.execute("SELECT * FROM LOG")
+            pprint(c.fetchall())
+
     def __repr__(self):
         return f"Logs('{self.userid}','{self.roomid}','{self.timein}','{self.timeout}')"
 
-def printfun(pdata):
+class Access_Control:
 
+    def __init__(self,timein,timeout,userid,srno=None,roomid=None):
+        self.srno = srno
+        self.userid = userid
+        self.roomid = roomid
+        self.timein = timein
+        self.timeout = timeout
 
-    return f""
+    #insert data to table access_system
+    def insert_access_control(ac,conn,c):
+        with conn:
+            c.execute("INSERT INTO ACCESS_CONTROL(userid,timein,timeout) VALUES(:userid,:timein,:timeout)",{'userid':ac.userid,'timein': ac.timein,'timeout': ac.timeout})
+
+    # display all data
+    def getAllACData(conn,c):
+        with conn:
+            c.execute("SELECT * FROM access_control")
+            pprint(c.fetchall())
--- Server/Database/database.py
+++ Server/Database/database.py
@@ -3,30 +3,93 @@ import sys
 from Models import *
 from pprint import pprint
 
-conn = sqlite3.connect('Access_Syetem.db')
+conn = sqlite3.connect('Access_System.db')
 c = conn.cursor()
+conn.execute("PRAGMA foreign_keys = ON")
+
+user1 =  Users(2220200309,'Abbas', 'A',"abbas", 1,'extc',132468579,1)
+room = Rooms(801,"phy",1)
+
+ac = Access_Control("12:41","1:45",1)
+
+
 
 # drop table if exists
 # c.execute('DROP TABLE IF EXISTS users')
 # c.execute('DELETE FROM room')
-# c.execute('DROP TABLE room')
+# c.execute('DROP TABLE access_control')
 
 
+#################################################### TABLE USERS #################################################
+
+try:
+    # create table Users
+    conn.execute('''CREATE TABLE IF NOT EXISTS USERS
+    (
+        id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+        uid integer NOT NULL,
+        fname text NOT NULL,
+        lname text NOT NULL,
+        passw text NOT NULL,
+        dept text NOT NULL,
+        phone text NOT NULL,
+        isStudent int NOT NULL
+    ) ''')
+
+except sqlite3.OperationalError :
+    pass
+
+    # Insert Data into table user
+    Users.insert_user(user1,conn,c)
+
+    # Delete Data of table user
+    # Users.delete_user(user1,conn)
+
+    # Update Data of table user
+    # Users.update_user(user1,conn)
+
+##################################################### TABLE ROOMS ################################################
+try:
+    # pass
+    #Create table Room
+    c.execute("""CREATE TABLE IF NOT EXISTS ROOMS
+        (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            roomid INTEGER NOT NULL,
+            roomname TEXT NOT NULL,
+            uid INTEGER NULL,
+            FOREIGN KEY (uid) 
+            REFERENCES USERS (id)
+        )
+    """)
+    
+except sqlite3.OperationalError :
+    pass
+    
+    # Insert Data Into table rooms
+    # Rooms.insert_rooms(room,conn,c)  
+
+    # Delete Data from table rooms
+    # Rooms.delete_rooms(room,conn)
+
+    # Update Data Into table rooms
+    # Rooms.update_rooms(room)
 
 #################################################### TABLE ACCESS_CONTROL #################################################
 
 try:
     # Create table Access_Control
-    c.execute("""CREATE TABLE access_control
+    c.execute("""CREATE TABLE IF NOT EXISTS ACCESS_CONTROL
         (
+        srno integer PRIMARY KEY AUTOINCREMENT NOT NULL,
         timein text not null,
         timeout text not null,
-        userid INTEGER NOT NULL,
-        roomid TEXT NOT NULL,
+        userid INTEGER NULL,
+        rid Integer NULL,
         FOREIGN KEY (userid) 
-        REFERENCES users (uid),
-        FOREIGN KEY (roomid) 
-        REFERENCES room (roomid)
+        REFERENCES USERS (uid),
+        FOREIGN KEY (rid) 
+        REFERENCES ROOMS (roomid)
         )
     """)
 
@@ -34,105 +97,33 @@ except sqlite3.OperationalError :
     pass
 
     # Insert data into Access_control
-    # def insert_access_control(ac):
-    #     with conn:
-    #         c.execute("INSERT INTO access_control(uid,fname,lname,dept,phone,isStudent,isProfessor) VALUES(:uid,:fname,:lname,:dept,:phoneno,:isStudent,:isProfessor)",{'uid': user.uid,'fname': user.fname,'lname': user.lname,'dept':user.dept,'phoneno': user.phoneno,'isStudent': user.isStudent,'isProfessor': user.isProfessor})
-
+    Access_Control.insert_access_control(ac,conn,c)
+        
     # Delete data from Access_control
 
     # Update data into Access_control
 
 
 
-## CRUD OPERARIONS
-
-#################################################### TABLE USERS #################################################
-
-try:
-    # create table Users
-    conn.execute('''CREATE TABLE users 
-    (
-        id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-        uid integer NOT NULL,
-        fname text NOT NULL,
-        lname text NOT NULL,
-        passw text null,
-        dept text NOT NULL,
-        phone text NULL,
-        isStudent int NOT NULL
-    ) ''')
-
-except sqlite3.OperationalError :
-
-    # Insert Data into table user
-    def insert_user(user):
-        with conn:
-            c.execute("INSERT INTO users(uid,fname,lname,passw,dept,phone,isStudent) VALUES(:uid,:fname,:lname,:passw,:dept,:phoneno,:isStudent)",{'uid': user.uid,'fname': user.fname,'lname': user.lname,'passw':user.passw,'dept':user.dept,'phoneno': user.phoneno,'isStudent': user.isStudent})
-
-    # Delete Data of table user
-    def delete_user(user):
-        with conn:
-            c.execute("DELETE from users WHERE uid = :uid",{'uid': user.uid})
-
-    # Update Data of table user
-    def update_user(user):
-        with conn:
-            c.execute(""" UPDATE users SET fname = :fname AND lname = :lname AND uid = :uid
-                    WHERE id = :id""",{':uid':user.uid,'id': user.id, 'fname': user.fname, 'lname': user.lname})
-
-##################################################### TABLE ROOMS ################################################
-try:
-    #Create table Room
-    c.execute("""CREATE TABLE room
-        (
-            id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-            roomid INTEGER NULL,
-            timein TEXT NOT NULL,
-            timeout TEXT NOT NULL,
-            uid INTEGER NULL,
-            FOREIGN KEY (timeout) 
-            REFERENCES access_control (timeout),
-            FOREIGN KEY (uid) 
-            REFERENCES user (id),
-            FOREIGN KEY (timein) 
-            REFERENCES access_control (timein)
-        )
-    """)
-except sqlite3.OperationalError :
-
-    # Insert Data Into table rooms
-    def insert_rooms(room):
-        with conn:
-            c.execute("INSERT INTO room(roomid,timein,timeout) VALUES(:roomid,:timein,:timeout)",{'roomid': room.roomid,'timein':room.timein,'timeout':room.timeout})
-
-    # Delete Data from table rooms
-    def delete_rooms(room):
-        with conn:
-            c.execute("DELETE from users WHERE uid = :uid",{'uid': room.roomid})
-
-    # Update Data Into table rooms
-    def update_rooms(room):
-        pass
-
 ##################################################### TABLE LOGS ################################################
 
 try:
     # Create Table Log
-    c.execute("""CREATE TABLE LOG 
+    c.execute("""CREATE TABLE IF NOT EXISTS LOG 
     ( 
         srno integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-        uid integer NULL,
-        roomid integer NULL,
+        uid integer NOT NULL,
+        roomid integer NOT NULL,
         timein integer NOT NULL,
         timeout integer NOT NULL,
         FOREIGN KEY (timein) 
-        REFERENCES room (timein),
+        REFERENCES ROOMS (timein),
         FOREIGN KEY (timeout) 
-        REFERENCES room (timeout),
+        REFERENCES ROOMS (timeout),
         FOREIGN KEY (uid) 
-        REFERENCES users (uid),
+        REFERENCES USERS (uid),
         FOREIGN KEY (roomid) 
-        REFERENCES room (roomid)
+        REFERENCES ROOMS (roomid)
     )
 """)
 except sqlite3.OperationalError :
@@ -140,15 +131,6 @@ except sqlite3.OperationalError :
 
 ##################################################### TABLE ENDS ################################################
 
-# c.execute("INSERT INTO users(uid,fname,lname,dept,phone,isStudent,isProfessor) VALUES(:uid,:fname,:lname,:dept,:phoneno,:isStudent,:isProfessor)",{'uid': user1.uid,'fname': user1.fname,'lname': user1.lname,'dept':user1.dept,'phoneno': user1.phoneno,'isStudent': user1.isStudent,'isProfessor': user1.isProfessor})
-
-# c.execute(f"INSERT INTO users(uid,fname,lname,dept,phone,isStudent,isProfessor) VALUES({user2.uid},{user2.fname},{user2.lname},{user2.dept},{user2.phoneno},{user2.isStudent},{user2.isProfessor})")
-
-# c.execute("DROP TABLE users")
-# arg1 = sys.argv[1]
-# arg2 = sys.argv[2]
-# arg3 = sys.argv[3]
-user1 =  Users('Abbas', 'A', 2220200309,'extc',1,132468579,1,0)
 user2 =  Users('Shinde', 'D', 2220200310,'extc',1,132468579,0,1)
 user3 =  Users('Chinmay', 'B', 2220200309,'extc',1,132468579,1,0)
 user4 =  Users('Yash', 'S', 2220200309,'extc',1,132468579,1,0)
@@ -157,7 +139,6 @@ user6 =  Users('Kavita', 'C', 2220200310,'extc',1,132468579,0,1)
 
 
 
-room = Rooms(801,"19:20","20:20")
 room1 = Rooms(802,"11:20","13:20")
 room2 = Rooms(803,"12:20","15:20")
 room3 = Rooms(804,"14:10","17:20")
@@ -182,30 +163,24 @@ conn.commit()
 
 ##################################################### GET DATA ################################################
 
-def getAllUsers():
-    c.execute(" SELECT * FROM users")
-    pprint(c.fetchall())
+Users.getAllUsers(conn,c)
 
-def getStudent():
-    c.execute(" SELECT * FROM users WHERE isStudent = 1")
-    pprint(c.fetchall())
+# Users.getStudent(conn)
 
-def getProfessor():
-    c.execute(" SELECT * FROM users WHERE isStudent = 0")
-    pprint(c.fetchall())
+# Users.getProfessor(conn)
 
-def getAllRooms():
-    c.execute(" SELECT * FROM room")
-    pprint(c.fetchall())
+Rooms.getAllRooms(conn,c)
 
-def getRoom(room):
-    c.execute(" SELECT * FROM room WHERE roomid = :roomid", {'roomid':room})
-    pprint(c.fetchall())
+# Rooms.getRoom(conn,801,c)
+
+Access_Control.getAllACData(conn,c)
 
 ##################################################### DISPLAY DATA ################################################
 
-getAllRooms()
-getAllUsers()
+# getAllRooms()
+# getAllUsers()
+# c.execute("SELECT * FROM room")
+# pprint(c.fetchall())
 
 
 conn.close()
