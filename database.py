f538d6fd44402e4fe4729190635667f6ed9bf96b
Chinmay Bhole <chinmaymotog5@gmail.com>
2021-12-29 14:24:35 +0530

    Clean the space

--- database.py
+++
@@ -1,4 +0,0 @@
-import sqlite3 as sql
-
-#conn = sql.connect(':memory:') create a database in memory andd disappear after we end the code
-conn = sql.connect('people.db') # Create the database
--- sqlite-try.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-# Create a connect object
-con = sqlite3.connect('test.db')
-# To save database a special path name :memory: can be provided to create a temporary database in RAM.
-
